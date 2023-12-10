2f03e0ff6ef084af1927770cfe1b04257f370831
Felicia Luo <129685045+felicialuo@users.noreply.github.com>
2023-12-09 23:07:22 -0500

    Add save recording

--- script.py
+++ script.py
@@ -1,15 +1,28 @@
 """
-Inference yolov3 in Realsense D435 camera
-Creator: Tony Do
-Date: 9nd July, 2021
-E-mail: vanhuong.robotics@gmail.com
-- updated object depth information
-Modified by Felicia Luo, Oct 2nd, 2023
+Real-time Inference yolov3 with Intel Realsense D435i camera
+Creator: Felicia Luo
+Date: 12/09/2023
+Based on Tony Do (vanhuong.robotics@gmail.com)'s Inference yolov3 in Realsense D435 camera
+
+Added save recorded color, depth, and annotated detection images
 """
 import pyrealsense2 as rs
 import numpy as np
 import cv2
 import sys
+import json
+from os.path import exists, join, abspath
+import os
+
+# Path Configs
+OUTPUT_FOLDER = '../output/rs_recording/'
+PATH_DEPTH  = OUTPUT_FOLDER + 'depth/'
+PATH_COLOR = OUTPUT_FOLDER + 'color'
+PATH_DET = OUTPUT_FOLDER + 'detection'
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+os.makedirs(PATH_DEPTH, exist_ok=True)
+os.makedirs(PATH_COLOR, exist_ok=True)
+os.makedirs(PATH_DET, exist_ok=True)
 
 # Initialize the parameters
 confThreshold = 0.5
@@ -107,6 +120,23 @@ def process_detection(frame, outs):
         y = int(top+ height/2)
         drawPredicted(classIds[i], confidences[i], left, top, left+width, top+height,frame,x,y)
 
+def save_intrinsic_as_json(filename, frame):
+    intrinsics = frame.profile.as_video_stream_profile().intrinsics
+    with open(filename, 'w') as outfile:
+        obj = json.dump(
+            {
+                'width':
+                    intrinsics.width,
+                'height':
+                    intrinsics.height,
+                'intrinsic_matrix': [
+                    intrinsics.fx, 0, 0, 0, intrinsics.fy, 0, intrinsics.ppx,
+                    intrinsics.ppy, 1
+                ]
+            },
+            outfile,
+            indent=4)
+
 if __name__ == "__main__":
     classes = None
     with open(classesFile, "rt") as f:
@@ -116,28 +146,50 @@ if __name__ == "__main__":
     net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-    print("118. net.getLayerNames()", net.getLayerNames())
+    # print("118. net.getLayerNames()", net.getLayerNames())
+
+    # Streaming loop
+    frame_count = 0
     try:
         while True:
             # Wait for a coherent pair of frames: depth and color
-            print("122. waiting for frames")
+            # print("122. waiting for frames")
             frames = pipeline.wait_for_frames()
-            print("124. got frames")
+            # print("124. got frames")
             depth_frame = frames.get_depth_frame()
             color_frame = frames.get_color_frame()
             if not depth_frame or not color_frame:
                 continue
+
             # Convert images to numpy arrays
             depth_image = np.asanyarray(depth_frame.get_data())
             color_image = np.asanyarray(color_frame.get_data())
             blob = cv2.dnn.blobFromImage(color_image, 1/255, (inpWidth, inpHeight), [0,0,0],1,crop=False)
             net.setInput(blob)
             outs = net.forward(getOutputsNames(net))
+
+            # Record color and depth images
+            if frame_count == 0:
+                save_intrinsic_as_json(
+                    join(OUTPUT_FOLDER, "camera_intrinsic.json"),
+                    color_frame)
+            cv2.imwrite("%s/%06d.png" % \
+                    (PATH_DEPTH, frame_count), depth_image)
+            cv2.imwrite("%s/%06d.jpg" % \
+                    (PATH_COLOR, frame_count), color_image)
+
             # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
             depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
             process_detection(color_image,outs)
             depth_colormap_dim = depth_colormap.shape
             color_colormap_dim = color_image.shape
+
+            # Save the annotated image with detection
+            cv2.imwrite("%s/%06d.jpg" % \
+                    (PATH_DET, frame_count), color_image)
+            print("Saved color + depth + detection %06d" % frame_count)
+            frame_count += 1
+
             # If depth and color resolutions are different, resize color image to match depth image for display
             if depth_colormap_dim != color_colormap_dim:
                 resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]),
@@ -145,6 +197,7 @@ if __name__ == "__main__":
                 images = np.hstack((resized_color_image, depth_colormap))
             else:
                 images = np.hstack((color_image, depth_colormap))
+
             # Show images
             cv2.imshow('Real-time YOLO with Depth', images)
             if cv2.waitKey(1) & 0xFF == ord('q'):
