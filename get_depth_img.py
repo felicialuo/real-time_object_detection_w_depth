86bb959e643231a0b269e310d0201a8330b519ea
Felicia Luo <129685045+felicialuo@users.noreply.github.com>
2024-05-01 20:15:25 -0400

    Update get_depth_img.py

--- get_depth_img.py
+++ get_depth_img.py
@@ -2,17 +2,18 @@ import numpy as np
 import cv2
 import os
 import matplotlib.pyplot as plt
+from tqdm import tqdm 
 
-depth_images = sorted([f for f in os.listdir("../figma_pres_imgs/exp2/depth")])
+depth_images = sorted([f for f in os.listdir("../datasets/event_20240405_18_06_48_fps1_clip_1_0/depth")])
 
 # video_path = "../figma_pres_imgs/exp2/depth_vidoe.avi"
 # fourcc = cv2.VideoWriter_fourcc(*'XVID')
 # out = cv2.VideoWriter(video_path, fourcc, 10, (640, 480))
 
 # Read the npz files and store the depth images in a list
-for img in depth_images:
+for img in tqdm(depth_images):
 
-    file_path = os.path.join("../figma_pres_imgs/exp2/depth", img)
+    file_path = os.path.join("../datasets/event_20240405_18_06_48_fps1_clip_1_0/depth", img)
     data = np.load(file_path)
     depth_image = data['arr_0']  # Assuming the array is stored with the default key 'arr_0'
 
@@ -20,9 +21,9 @@ for img in depth_images:
     max_value = depth_image.max()
     normalized_array = ((depth_image - min_value) / (max_value - min_value)) * 255
     normalized_array_int = normalized_array.astype(np.uint8)
-    print(normalized_array_int.shape)
+    # print(normalized_array_int.shape)
 
-    cv2.imwrite(os.path.join("../figma_pres_imgs/exp2/depth_img", img.replace("npz", "jpg")), normalized_array_int)
+    cv2.imwrite(os.path.join("../datasets/event_20240405_18_06_48_fps1_clip_1_0/depth_img", img.replace("npz", "jpg")), normalized_array_int)
 
     # plt.imshow(normalized_array_int, cmap='gray')
     # plt.show()
